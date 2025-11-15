11f2447801b3a6180368e641d7532292a94cb741
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-11-15 22:29:54 +0600

    User & Admin Creation: Part 2

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express, { Application, Request, Response } from "express";
-import cors from 'cors';
+import cors from "cors";
 import { userRoutes } from "./app/modules/User/user.routes";
 
 const app: Application = express();
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph Health care server..."
-    })
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Ph Health care server...",
+  });
 });
 
-app.use('/api/v1/user', userRoutes)
+app.use("/api/v1/user", userRoutes);
 
 export default app;
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,7 +1,27 @@
-const createAdmin = async () => {
-  return {
-    message: "Admin Created",
+import { PrismaClient, UserRole } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const createAdmin = async (data: any) => {
+  const userData = {
+    email: data.admin.email,
+    password: data.password,
+    role: UserRole.ADMIN,
   };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    const createUserData = await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createAdminData = await transactionClient.admin.create({
+      data: data.admin,
+    });
+
+    return createAdminData;
+  });
+
+  return result;
 };
 
 export const userService = {
