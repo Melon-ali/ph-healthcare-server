61e6ed12c24cb91472b6fceb095287b84a350454
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-12-07 18:33:57 +0600

    Developing Reusable Middleware and Defining Types for Filtering Data and Pagination Options

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,19 +1,11 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
 
-const catchAsync = (fn: RequestHandler) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await fn(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
-};
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
   const filters = pick(req.query, adminFilterableFields);
--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { AdminController } from "./admin.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { adminValidationSchemas } from "./admin.validations";
 
 const router = express.Router();
 
 router.get("/", AdminController.getAllFromDB);
 router.get("/:id", AdminController.getByIdFromDb);
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch(
+  "/:id",
+  validateRequest(adminValidationSchemas.update),
+  AdminController.updateIntoDB
+);
 router.delete("/:id", AdminController.deleteFromDB);
 router.delete("/soft/:id", AdminController.softDeleteFromDB);
 
