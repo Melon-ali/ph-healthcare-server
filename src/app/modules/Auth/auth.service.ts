2b3ef2fba8efd60aaecad2aab0007ff879c85d3c
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-12-09 17:28:46 +0600

    Building jwtHelper for Token Generation

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,37 +1,40 @@
+import { jwtHelpers } from "../../../helpars/jwtHelpers";
 import prisma from "../../../shared/prisma";
 import * as bcypt from "bcrypt";
-import * as jwt from "jsonwebtoken";
 
 const loginUser = async (payload: { email: string; password: string }) => {
   const uesrData = await prisma.user.findFirstOrThrow({
     where: {
-        email: payload.email,
-    }
+      email: payload.email,
+    },
   });
 
-  const isCorrectPassword: boolean = await bcypt.compare(payload.password, uesrData.password);
+  const isCorrectPassword: boolean = await bcypt.compare(
+    payload.password,
+    uesrData.password
+  );
 
   if (!isCorrectPassword) {
     throw new Error("Password is incorrect");
   }
 
-  const accessToken = jwt.sign({
-    email: uesrData.email,
-    role: uesrData.role,
-  }, 'abcdefg', 
-{
-    algorithm: 'HS256',
-    expiresIn: '5m',
-})
+  const accessToken = jwtHelpers.generateToken(
+    {
+      email: uesrData.email,
+      role: uesrData.role,
+    },
+    "abcdefg",
+    "5m"
+  );
 
-const refreshToken = jwt.sign({
-    email: uesrData.email,
-    role: uesrData.role,
-  }, 'abcdefgh', 
-{
-    algorithm: 'HS256',
-    expiresIn: '30m',
-})
+  const refreshToken = jwtHelpers.generateToken(
+    {
+      email: uesrData.email,
+      role: uesrData.role,
+    },
+    "abcdefgh",
+    "30d"
+  );
 
   return {
     accessToken,
