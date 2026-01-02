8fbaffcc91ab1d524c7535bd65bf0fceaf4a0a45
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2026-01-02 11:24:59 +0600

    Creating Reusable Authentication Middleware and Handling API Errors

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { jwtHelpers } from "../../helpars/jwtHelpers";
+import config from "../config";
+import { Secret } from "jsonwebtoken";
+import ApiError from "../errors/ApiError";
+import httpStatus from "http-status";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Authentication logic here
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
+      }
+
+      const verifiedUser = jwtHelpers.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret
+      );
+      console.log(verifiedUser);
+
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new ApiError(httpStatus.FORBIDDEN, "FORBIDDEN!");
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default auth;
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { userController } from "./user.controller";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
-router.post("/", userController.createAdmin);
+
+router.post("/", auth("admin"), userController.createAdmin);
 
 export const userRoutes = router;
